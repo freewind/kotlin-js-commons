98682ac30fd2e8106f36f5b83e87f4d4dedbd2f6
freewind <nowindlee@gmail.com>
2018-07-17 23:41:26 +0800

    fix js name, version -> 0.1.1

--- generated/kotlin-js-commons.js
+++ generated/kotlin-js-commons.js
@@ -1,7 +1,7 @@
 if (typeof kotlin === 'undefined') {
-  throw new Error("Error loading module 'hello'. Its dependency 'kotlin' was not found. Please, check whether 'kotlin' is loaded prior to 'hello'.");
+  throw new Error("Error loading module 'kotlin-js-commons'. Its dependency 'kotlin' was not found. Please, check whether 'kotlin' is loaded prior to 'kotlin-js-commons'.");
 }
-var hello = function (_, Kotlin) {
+this['kotlin-js-commons'] = function (_, Kotlin) {
   'use strict';
   var $$importsForInline$$ = _.$$importsForInline$$ || (_.$$importsForInline$$ = {});
   var defineInlineFunction = Kotlin.defineInlineFunction;
@@ -13,7 +13,7 @@ var hello = function (_, Kotlin) {
     x['key2'] = 'value2';
     console.log(x);
   }
-  var JsObj = defineInlineFunction('hello.kotlinjs.common.JsObj_287e2$', wrapFunction(function () {
+  var JsObj = defineInlineFunction('kotlin-js-commons.kotlinjs.common.JsObj_287e2$', wrapFunction(function () {
     var Any = Object;
     var throwCCE = Kotlin.throwCCE;
     return function () {
@@ -21,13 +21,13 @@ var hello = function (_, Kotlin) {
       return (tmp$ = {}) == null || Kotlin.isType(tmp$, Any) ? tmp$ : throwCCE();
     };
   }));
-  var JsObj_0 = defineInlineFunction('hello.kotlinjs.common.JsObj', wrapFunction(function () {
+  var JsObj_0 = defineInlineFunction('kotlin-js-commons.kotlinjs.common.JsObj', wrapFunction(function () {
     var json = Kotlin.kotlin.js.json_pyyo18$;
     return function () {
       return json([]);
     };
   }));
-  $$importsForInline$$.hello = _;
+  $$importsForInline$$['kotlin-js-commons'] = _;
   var package$example = _.example || (_.example = {});
   package$example.main_kand9s$ = main;
   var package$kotlinjs = _.kotlinjs || (_.kotlinjs = {});
@@ -35,6 +35,6 @@ var hello = function (_, Kotlin) {
   package$common.JsObj_287e2$ = JsObj;
   package$common.JsObj = JsObj_0;
   main([]);
-  Kotlin.defineModule('hello', _);
+  Kotlin.defineModule('kotlin-js-commons', _);
   return _;
-}(typeof hello === 'undefined' ? {} : hello, kotlin);
+}(typeof this['kotlin-js-commons'] === 'undefined' ? {} : this['kotlin-js-commons'], kotlin);
